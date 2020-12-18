a85d3e67f009e038631e3fe6bd0edb3b35e4df69
Amir Saboury <amir.saboury@gmail.com>
2020-12-17 22:11:58 -0500

    initial

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,105 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Head from 'next/head';
+import { useEffect, useState } from 'react';
+import styles from '../styles/Home.module.css';
+
+const getAge = () =>
+  Math.ceil(
+    (new Date().getTime() - new Date('1990-04-14').getTime()) / (60 * 1000)
+  );
+
+const Leenk = ({ type, href, children }) => {
+  let classes = '';
+  if (type === 'social') {
+    classes = 'px-2 py-1 rounded-sm';
+  } else if (type === 'cool') {
+    classes = 'px-1 py-1 rounded-sm';
+  }
+
+  return (
+    <a className={`${type} ${classes}`} href={href}>
+      {children}
+    </a>
+  );
+};
 
 export default function Home() {
+  const [minutes, setMinutes] = useState(getAge());
+
+  useEffect(() => {
+    const id = setInterval(() => {
+      setMinutes(getAge());
+    }, 60 * 1000);
+
+    return () => {
+      clearInterval(id);
+    };
+  }, []);
+
   return (
-    <div className={styles.container}>
+    <div className='flex'>
       <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>Amir Saboury's home page</title>
       </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+      <main className='flex flex-col mx-auto p-2 m-2'>
+        <div className='sm:w-2/3 md:w-1/2 lg:w-1/3 flex justify-around p-3 m-3 mx-auto'>
+          <img className='rounded-full' src='/amir.saboury1.bw.jpg'></img>
+        </div>
+        <div className='sm:w-2/3 md:w-2/3 lg:w-1/2 border-t border-gray-200 p-4 m-4 mx-auto text-center'>
+          <h1 className='mb-1'>Hello 👋</h1>
+          <h5 className='mb-4'>My name is Amir Saboury!</h5>
+        </div>
+        <div className='p-2 m-2 mx-auto'>
+          <h3 className='mb-2'>
+            I'm currently located in Montréal.
+            <br />I have been alive for around{' '}
+            <Leenk
+              href='https://howlongagogo.com/date/1990/april/14'
+              type='cool'
+            >
+              {minutes.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')} minutes
+            </Leenk>{' '}
+            now.
+          </h3>
+          <h3 className='leading-normal mb-2'>
+            I{' '}
+            <Leenk href='https://amirs.dev' type='social'>
+              write code
+            </Leenk>{' '}
+            as a staff developer at{' '}
+            <a className='link' href='https://shopify.com'>
+              Shopify
+            </a>
+            .
+          </h3>
+          <h3 className='mb-2'>
+            I'm apparently a{' '}
+            <Leenk href='https://www.enneagraminstitute.com/type-9' type='cool'>
+              Peacemaker
+            </Leenk>{' '}
+            & an{' '}
+            <Leenk href='https://www.ipersonic.com/type/AT.html' type='cool'>
+              Analytical Thinker
+            </Leenk>{' '}
+            .
+          </h3>
+          <h3 className='leading-normal'>
+            I also{' '}
+            <Leenk href='https://www.instagram.com/amir.saboury' type='social'>
+              take photos
+            </Leenk>
+            ,{' '}
+            <Leenk href='https://twitter.com/as32' type='social'>
+              write tweets
+            </Leenk>{' '}
+            and answer to{' '}
+            <Leenk href='mailto:amir.saboury@gmail.com' type='social'>
+              email
+            </Leenk>
+            .
+          </h3>
         </div>
       </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
     </div>
-  )
+  );
 }
---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  purge: ['./pages/**/*.js', './components/**/*.js'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    fontFamily: {
+      main: [
+        'Inter',
+        '-apple-system',
+        'BlinkMacSystemFont',
+        'Segoe UI',
+        'Roboto',
+        'Oxygen',
+        'Ubuntu',
+        'Cantarell',
+        'Fira Sans',
+        'Droid Sans',
+        'Helvetica Neue',
+        'sans-serif',
+      ],
+    },
+
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
